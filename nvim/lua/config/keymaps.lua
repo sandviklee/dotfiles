42844ef7c59d0b5c698bfc5a8a0e86fbe7afaf85
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-12-14 12:08:44 +0100

    feat: Add nvim config

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("n", "<C-d>", "<C-d>zz", { desc = "Jump to the middle of the screen" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Jump to the middle of the screen" })
---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/sandviklee/.config/omarchy/current/theme/neovim.lua
